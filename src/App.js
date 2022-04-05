2578b1424484b67d2bc753fa6690f8b9ccf96cc3
hanomag123 <87242345+hanomag123@users.noreply.github.com>
2022-04-05 19:14:59 +0300

    library

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import {Dashboard} from './components/Dashboard/Dashboard';
 import {Header} from './components/Header/Header';
 import { Library } from './components/Library/Library';
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { useState } from 'react';
 
 export function App() {
+  const [library, setLibrary] = useState(JSON.parse(localStorage.getItem('library')) || [])
+  console.log(library)
   return (
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/library' element={<Library />} />
+        <Route path='/library' element={<Library library={library} setLibrary={setLibrary} />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Library/Library.jsx
+++ src/components/Library/Library.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styles from './Library.module.css'
-import PlayButton from '../../assets/img/play.svg'
+import addBtn from './../../assets/img/add.svg'
+import deleteBtn from './../../assets/img/delete.svg'
 
-export const Library = () => {
+export const Library = ({library, setLibrary}) => {
+    const inputValue = useRef();
+    const deleteWord = (id) => {
+        const updateLibrary = library.filter((word, index) => index !== id);
+        setLibrary(updateLibrary)
+        localStorage.setItem('library', JSON.stringify(updateLibrary));
+    }
+
+    const addNewWord = async (event) => {
+        event.preventDefault();
+        const response = await fetch(`http://tmp.myitschool.org/API/translate/?source=ru&target=en&word=${inputValue.current.value}`);
+        const translation = await response.json();
+        const updateLibrary = [...library, {word: translation.word, translate: translation.translate, learn: 0}];
+        setLibrary(updateLibrary);
+        localStorage.setItem('library', JSON.stringify(updateLibrary));
+        inputValue.current.value = '';
+    }
     return (
-        <section className={styles}>
-            Library
+        <section className={styles.libraryBlock}>
+            <span>Add new <b>Word</b></span>
+
+            <form onSubmit={addNewWord} className={styles.addWordBlock}>
+                <input ref={inputValue} type='text' />
+
+                <button>
+                    <img src={addBtn} alt='addButton'/>
+                </button>
+            </form>
+
+            <div className={styles.wordsTable}>
+                <ul>
+                    <li>
+                        <h3>Word</h3>
+                    </li>
+                    <li>
+                        <h3>Translation</h3>
+                    </li>
+                    <li>
+                        <h3>Learn</h3>
+                    </li>
+                </ul>
+
+                {library.map((word, index) => (
+                    <ul key={index}>
+                        <li>{word.word}</li>
+                        <li>{word.translate}</li>
+                        <li>{word.learn}</li>
+
+                        <div className={styles.settings}>
+                            <button onClick={() => deleteWord(index)}>
+                                <img src={deleteBtn} alt="deleteButton" />
+                            </button>
+                        </div>
+                    </ul>
+                ))}
+            </div>
         </section>
     )
 }
